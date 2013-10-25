32513d531ac7ea49d5bfe20bc9ecf50a5652112b
mikera <mike.r.anderson.13@gmail.com>
2013-10-25 18:52:47 +0800

    updates

--- bench.py
+++ bench.py
@@ -4,11 +4,32 @@ import numpy as np
 
 print ("Hello World with NumPy")
 
+def bench(name, params):
+	n = 10000
+	r = timeit.timeit(name+"("+params+")", setup="from __main__ import "+name, number=n)
+	return r * 1000000000.0 / n
+
+# elementwise building of a 10x10 matrix, followed by computation of the sum
+
 def buildsum(n): 
-	a = np.arange(n*n)
-	a = a.reshape([n,n])
-	a = np.sinh(a)
+	a = np.empty([n,n])
+	for i in range(n):
+		for j in range(n):
+			a[i,j] = i*j
+	a = np.sum(a)
 	return a
 
 print ("Timing buildsum")
-print(timeit.timeit("buildsum(30)", setup="from __main__ import buildsum"))
+print(bench("buildsum", "10"))
+
+
+# Multiplication (inner product) of two 20x20 matrices
+
+def multiply(n): 
+	a = np.empty([n,n])
+	b = np.empty([n,n])
+	a = np.dot (a,b)
+	return a
+	
+print ("Timing multiply")
+print(bench("multiply", "100"))
