37cff74eb4a5060ed3fbefb78f7e7a0d2cc59700
mikera <mike.r.anderson.13@gmail.com>
2013-11-15 03:50:47 -0500

    numpy benchmark edits

--- bench.py
+++ bench.py
@@ -28,17 +28,20 @@ print(bench("buildsum", "10"))
 # => about 0.86 ms
 
 def multiply(n): 
-	a = np.zeros([n,n])
-	b = np.zeros([n,n])
+	a = np.ones([n,n])
+	a.fill (0.5)
+	b = np.ones([n,n])
+	b.fill (0.5)
 	a = np.dot (a,b)
 	return a
 	
-print ("Timing multiply 100x100")
-print(bench("multiply", "100"))
+for x in [2,3,4,5,6,7,8,10,13,16,20,25,32,40,50,64,80,101,128,161,203,256,322,406,512,645,812,1024]:
+	# print ("Timing multiply 100x100")
+	print(str(x) + "\t"+ str(bench("multiply", str(x))))
 
 
 # Multiplication (inner product) of two 10x10 matrices
 # => about 6,400 ns
 
 print ("Timing multiply 10x10")
-print(bench("multiply", "10"))
+print (bench("multiply", "10"))
